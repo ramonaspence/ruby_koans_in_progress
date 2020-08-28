0d087a6412ce9d7f870b4455132784255e5438ea
RamonaEJSpence <ramonaejspence@gmail.com>
2020-08-28 10:36:45 -0400

    first commit

---
+++ koans/about_nil.rb
@@ -0,0 +1,44 @@
+require File.expand_path(File.dirname(__FILE__) + '/neo')
+
+class AboutNil < Neo::Koan
+  def test_nil_is_an_object
+    assert_equal true, nil.is_a?(Object), "Unlike NULL in other languages"
+  end
+
+  def test_you_dont_get_null_pointer_errors_when_calling_methods_on_nil
+    # What happens when you call a method that doesn't exist.  The
+    # following begin/rescue/end code block captures the exception and
+    # makes some assertions about it.
+    begin
+      nil.some_method_nil_doesnt_know_about
+    rescue Exception => ex
+      # What exception has been caught?
+      assert_equal NoMethodError, ex.class
+
+      # What message was attached to the exception?
+      # (HINT: replace __ with part of the error message.)
+      assert_match(/undefined method/, ex.message)
+    end
+  end
+
+  def test_nil_has_a_few_methods_defined_on_it
+    assert_equal true, nil.nil?
+    assert_equal "", nil.to_s
+    assert_equal "nil", nil.inspect
+
+    # THINK ABOUT IT:
+    #
+    # Is it better to use
+    #    obj.nil?
+    # or
+    #    obj == nil
+    # Why?
+
+    # I think `obj.nil?` 
+    # After reading this: https://stackoverflow.com/questions/1972266/obj-nil-vs-obj-nil
+    # It seems that obj.nil? actually the #.nil? method, while obj == nil checks obj against
+    # the nil singleton. The performance of using the #.nil? method is quicker in all scenarios according to
+    # https://stackoverflow.com/a/37287459/13984303
+  end
+
+end
